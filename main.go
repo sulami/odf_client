a88f5a20e211edac5966577f44bc934f2478a2ae
Robin Schroer <sulami@peerwire.org>
2015-02-15 16:10:27 +0100

    interact with the server

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"net"
+	"strconv"
+)
+
 func main() {
+	ip := flag.String("ip", "localhost", "Server IP to connect to")
+	port := flag.Int("port", 1339, "Port to use on the server")
+	address := *ip + ":" + strconv.Itoa(*port)
+
+	fmt.Println("Connecting to", address + "...")
+	conn, err := net.Dial("tcp", address)
+	if err != nil {
+		fmt.Println("Error: could not connect to the server")
+		return
+	}
+
+	fmt.Println("Connected to", conn.RemoteAddr())
+
+	fmt.Println("Logging out...")
+	_, err = conn.Write([]byte("LOGOUT"))
+	if err != nil {
+		fmt.Println("Error: could not connect to the server")
+		return
+	}
+
+	fmt.Println("Logged out")
 }
 
