775136a162f0ba02a6bc5b53c19bdfdc79344e64
Robin Schroer <sulami@peerwire.org>
2015-02-16 12:14:16 +0100

    take credentials from args and try to login

--- main.go
+++ main.go
@@ -12,6 +12,8 @@ import (
 func main() {
 	ip := flag.String("ip", "localhost", "Server IP to connect to")
 	port := flag.Int("port", 1339, "Port to use on the server")
+	username := flag.String("user", "sulami", "Username to login")
+	password := flag.String("password", "", "Password to login")
 	address := *ip + ":" + strconv.Itoa(*port)
 
 	fmt.Println("Connecting to", address + "...")
@@ -23,8 +25,8 @@ func main() {
 
 	fmt.Println("Connected to", conn.RemoteAddr())
 
-	fmt.Println("Logging out...")
-	_, err = conn.Write([]byte("LOGOUT"))
+	fmt.Println("Logging in...")
+	_, err = conn.Write([]byte("LOGIN " + *username + " " + *password))
 	if err != nil {
 		fmt.Println("Error: could not connect to the server")
 		return
